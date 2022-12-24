3abe0e6ebb9d60bfb4749d80e89f4a48956e81ce
vikashofficial94 <96981040+vikashofficial94@users.noreply.github.com>
2022-12-25 00:20:07 +0530

    Create PrintTheElement1.java

---
+++ PrintTheElement1.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class PrintTheElement1 {
+    public static void main(String args[]) {
+        Stack<Integer> stack = new Stack<>();
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        while (n-- > 0)
+            stack.push(s.nextInt());
+        printFifthElementFromStart(stack);
+    }
+
+    // Method to print the fifth element from the bottom of the stack
+    static void printFifthElementFromStart(Stack<Integer> stack) {
+        int n = stack.size();
+        // If the stack doesn't have enough elements
+        if (n < 5)
+            System.out.println("There are not enough elements in the stack");
+        else {
+            while (stack.size() > 5){
+                stack.pop();
+                 }
+            System.out.println(stack.peek());
+        }
+    }
+}
